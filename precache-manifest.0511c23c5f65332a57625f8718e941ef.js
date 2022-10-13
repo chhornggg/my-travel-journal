692f680007ca36b372adeaa0a009c411142dd927
Seng E Chhorng <chhorng@Sengs-MacBook-Pro.local>
2022-10-13 11:55:44 +0700

    Updates

---
+++ precache-manifest.0511c23c5f65332a57625f8718e941ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a13b43943c072fae6b4",
+    "url": "/my-travel-journal/static/js/main.1a13b439.chunk.js"
+  },
+  {
+    "revision": "6d25583a32abfa5e02e2",
+    "url": "/my-travel-journal/static/js/1.6d25583a.chunk.js"
+  },
+  {
+    "revision": "84002eb5bd3c439cca0a",
+    "url": "/my-travel-journal/static/js/runtime~main.84002eb5.js"
+  },
+  {
+    "revision": "ed7f6f85110dd7423cce07f0e394f5b1",
+    "url": "/my-travel-journal/static/media/Fill213.ed7f6f85.svg"
+  },
+  {
+    "revision": "d472343834de0a6489e6335d3a686a8a",
+    "url": "/my-travel-journal/static/media/Fill219.d4723438.svg"
+  },
+  {
+    "revision": "0aa8a7c70e2582691df31e36615471c0",
+    "url": "/my-travel-journal/index.html"
+  }
+];
